4c0596a5b35deed14f6ced2be7885910630a3910
Zenas Muli <zenas2294@gmail.com>
2019-05-04 11:56:06 +0300

    Added a search button to test api functionality. Test was successful

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Response, Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gitsearch';
+  reposArray:any;
+
+
+  link = "https://api.github.com/users/";
+
+  access_token = "?access_token=bf498cba8caba4b97a654f7da329cadebbf88cf7";
+  user = [];
+  repos = [];
+
+  constructor(private http:Http) {}
+  searchHub(search: HTMLInputElement){
+    let userlink = this.link + search.value + this.access_token;
+    let repolink = this.link + search.value + "/repos" + this.access_token;
+    this.http.get(userlink).subscribe((res:Response)=>{
+      this.user = res.json();
+      console.log(this.user);
+    })
+    this.http.get(repolink).subscribe((res2:Response)=>{
+      this.repos = res2.json();
+      console.log(this.repos);
+    })
+
+  }
+
+  ngOninit(){
+  //  this.http.get("https://api.github.com/users/zenas22?access_token=bf498cba8caba4b97a654f7da329cadebbf88cf7").subscribe((res:Response)=>{
+  //  console.log(res.json());
+  // })
+  }
 }
