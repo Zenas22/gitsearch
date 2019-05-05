0961f1af9ab9ef6ccd111d2b86c6b885d181b5cb
Zenas Muli <zenas2294@gmail.com>
2019-05-05 10:02:19 +0300

    Created user class

--- src/app/creator.pipe.ts
+++ src/app/creator.pipe.ts
@@ -5,8 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CreatorPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value:any): number {
+    let today:any = new Date();
+
+    var dateDifference = Math.abs(today - value)
+    const secondsPerDay = 86400;
+
+    var dateDifferenceSeconds = dateDifference * 0.001;
+
+    var dateCount = Math.round(dateDifferenceSeconds/secondsPerDay);
+
+    if (dateCounter >= 1){
+      return dateCount - 1;
+    }else{
+      return 0;
+    }
   }
 
 }
