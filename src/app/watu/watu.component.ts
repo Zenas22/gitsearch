1d8ba062c1681c0056948c4339fe32256e78af71
Zenas Muli <zenas2294@gmail.com>
2019-05-05 11:24:59 +0300

    Fixed card to display user in watu's html and set the ts file to validate its functionality

--- src/app/watu/watu.component.ts
+++ src/app/watu/watu.component.ts
@@ -1,13 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+import { UsersService } from '../huduma/users';
+import { User } from '../darasa/user';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-watu',
   templateUrl: './watu.component.html',
+  providers: [UsersService],
   styleUrls: ['./watu.component.css']
 })
 export class WatuComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  userName: string;
+
+  constructor( private userFindService: UsersService) {
+    this.userFindService.userName = "Zenas22";
+    this.userFindService.findUser();
+    this.user = this.userFindService.user;
+  }
+
+  searchUser(){
+    this.userFindService.searchUser(this.username);
+    this.userFindService.findUser();
+    this.user = this.userFindService.user;
+  }
 
   ngOnInit() {
   }
